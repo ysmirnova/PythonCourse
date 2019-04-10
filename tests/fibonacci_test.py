f58b751f138ac0ab16c1125869c4d1ac4edb850f
ysmirnova <ext-yuliia.smirnova@here.com>
2019-04-10 12:27:42 +0300

    add fibonacci test

---
+++ tests/fibonacci_test.py
@@ -0,0 +1,15 @@
+from utils import Fibo
+
+
+class TestFibbonacci:
+    def test_fibo_zero(self):
+        assert Fibo.generatefibonacci(0) == 0
+
+    def test_fibo_first(self):
+        assert Fibo.generatefibonacci(1) == 1
+
+    def test_fibo_second(self):
+        assert Fibo.generatefibonacci(2) == 1
+
+    def test_fibo_oneHundred(self):
+        assert Fibo.generatefibonacci(100) == 354224848179261915075
