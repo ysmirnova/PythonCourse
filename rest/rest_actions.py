21800ed7e5ae61182e46772f399d17e717a34bfc
Julia <ysmirnova@lohika.com>
2019-05-30 01:51:22 +0300

    api

---
+++ rest/rest_actions.py
@@ -0,0 +1,37 @@
+import json
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class RestActions:
+    HEADERS = {'content-type': 'application/json'}
+    HOST = None
+    AUTH = None
+
+    def __init__(self, host, user_name, user_pass):
+        self.HOST = host
+        self.AUTH = HTTPBasicAuth(user_name, user_pass)
+
+    def login_rest(self, path):
+        response = requests.get(self.HOST + path, auth=self.AUTH, headers=self.HEADERS)
+        try:
+            msg = response.json()['name']
+        except json.decoder.JSONDecodeError as err:
+            msg = response.text
+
+        return response.status_code, msg
+
+    def create_issue_rest(self, path, issue_data):
+        key_issue = None
+        id_issue = None
+        error = None
+        response = requests.post(self.HOST + path, auth=self.AUTH, data=issue_data, headers=self.HEADERS)
+        if response.status_code == 201:
+            key_issue = response.json()['key']
+            id_issue = response.json()['id']
+        else:
+            error = response.json()['errors']['summary']
+        return response.status_code, key_issue, id_issue, error
+
+
--- tests/api/login_test.py
+++ tests/api/login_test.py
@@ -1,24 +1,41 @@
+import json
+import os
+
 import pytest
 
-from rest.login import RestLogin
+from rest.rest_actions import RestActions
 
 
 class TestLogin:
+    HOST = 'https://jira.hillel.it'
+    USER = 'YuliiaSmirnova'
+    PASSWORD = 'YuliiaSmirnova'
 
-    @pytest.mark.parametrize("user, password, expected",
-                             [("incorrect", "YuliiaSmirnova", 401),
-                              ("YuliiaSmirnova", "incorrect", 401),
-                              ("YuliiaSmirnova", "YuliiaSmirnova", 200)])
-    def test_login(self, user, password, expected):
-        rest_login = RestLogin()
-        url = 'https://jira.hillel.it/rest/auth/1/session'
-        rest_login.login_rest(url, user, password)
+    @pytest.mark.parametrize("user, password, expected_status_code, expected_response_msg",
+                             [("incorrect", "YuliiaSmirnova", 401, "Unauthorized"),
+                              ("YuliiaSmirnova", "incorrect", 401, "Unauthorized"),
+                              ("YuliiaSmirnova", "YuliiaSmirnova", 200, "YuliiaSmirnova")])
+    def test_login(self, user, password, expected_status_code, expected_response_msg):
+        path = '/rest/auth/1/session'
+        rest_actions = RestActions(self.HOST, user, password)
+        status_code, response_msg = rest_actions.login_rest(path)
+        assert status_code == expected_status_code
+        assert expected_response_msg in response_msg
 
-    def test_create_issue(self):
-        rest_login = RestLogin()
-        url = 'https://jira.hillel.it/rest/api/2/issue'
-        rest_login.create_issue_rest(url, "YuliiaSmirnova", "YuliiaSmirnova")
-
+    @pytest.mark.parametrize("data_file, expected_status_code, expected_key, expected_error",
+                             [("issue_full_field.json",  201, "WEBINAR", None),
+                              ("issue_requared_field.json", 201, "WEBINAR", None),
+                              ("issue_long_name.json", 400, None, "Summary must be less than 255 characters.")])
+    def test_create_issue(self, data_file, expected_status_code, expected_key, expected_error):
+        path = '/rest/api/2/issue'
+        rest_actions = RestActions(self.HOST, self.USER, self.PASSWORD)
+        json_path = os.path.join(os.getcwd(), data_file)
+        with open(json_path) as json_file:
+            issue_data = json.load(json_file)
+        status_code, key_issue, id_issue, error = rest_actions.create_issue_rest(path, json.dumps(issue_data))
+        assert status_code == expected_status_code
+        assert expected_key in key_issue
+        assert expected_error == error
 
 
 
